74c6d207695b12b06808d60a623732cbea33a1fa
jialiw1370 <jw2106@georgetown.edu>
2025-12-15 12:33:59 -0500

    if/while+operations

---
+++ basics-1.py
@@ -0,0 +1,43 @@
+PS C:\Users\liliu> py
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a1 = 10
+>>> if a1 >100:
+...  print(11)
+... else:
+...  print(10)
+... a1
+...
+10
+10
+>>> a1 = 10
+>>> if a1 >100:
+...  print(11)
+... else:
+...  print(10)
+...
+10
+>>> a1 = 10
+>>> while a1 <50:
+...  a1 = a1 +2**3/2
+... print("more than 50")
+...
+more than 50
+>>> a1 = 10
+>>> while a1 <50:
+...  a1 = a1 +2 ** 3/2
+...  print(a1)
+... print("more than 50")
+...
+14.0
+18.0
+22.0
+26.0
+30.0
+34.0
+38.0
+42.0
+46.0
+50.0
+more than 50
+>>> exit()
