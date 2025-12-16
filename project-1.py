f4012e2ce6ff3ff24aad50b7f60e8d50ab4e01ff
jialiw1370 <jw2106@georgetown.edu>
2025-12-16 11:51:49 -0500

    basic skincare decision

---
+++ project-1.py
@@ -0,0 +1,11 @@
+
+x = "to brighten skin"
+if x == "to brighten skin":
+  print("Vitamin C or Niacinamide")
+elif x == "to moisturize skin":
+  print("Hyaluronic Acid")
+elif x == "to reduce wrinkles":
+  print("Retinol")
+
+
+
