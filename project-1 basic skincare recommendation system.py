5729cf8a7f181c9b17b77f8bbb6158c57ca2d163
jialiw1370 <jw2106@georgetown.edu>
2025-12-17 14:46:03 -0500

    skincare recommendation system using dictionary, and if/else

---
+++ project-1 basic skincare recommendation system.py
@@ -0,0 +1,22 @@
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+
+goal = input("What is your skincare goal? ")
+
+recommendations = {
+"brighten" : "Vitamin C or Niacinamide",
+"moisturize" : "Hyaluronic Acid",
+"wrinkles" : "Retinol"}
+recommendation = None
+for keyword, rec in recommendations.items():
+    if keyword in goal:
+        recommendation = rec
+if recommendation:
+    print(recommendation)
+else:
+    print("No specific recommendation found. Try: 'To brighten skin', 'to moisturize skin', 'to reduce wrinkles'.")
+
+
+
+
