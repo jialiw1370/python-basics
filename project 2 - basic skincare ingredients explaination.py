c2fde6a9332f8cf4ad77963a46b2dce0b4f68341
jialiw1370 <jw2106@georgetown.edu>
2025-12-18 22:10:33 -0500

    explain ingredients using def()

---
+++ project 2 - basic skincare ingredients explaination.py
@@ -0,0 +1,15 @@
+
+def skincare(i):
+    if i == "Vitamin C":
+        return "Vitamin C brightens your skin"
+    if i == "Retinol":
+        return "Retinol reduces your wrinkles"
+    if i == "Hyaluronic Acid":
+        return "Hyaluronic Acid moisturizes your skin"
+    if i == "Niacinamide":
+        return "Niacinamide brightens your skin"
+    else:
+        return "Try again! Try typing 'Vitamin C', 'Retinol' etc"
+
+skincare("Retinol")
+'Retinol reduces your wrinkles'
