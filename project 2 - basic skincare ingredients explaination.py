66c98a1895c7759b856b13e7a8e22080e7e9d360
jialiw1370 <jw2106@georgetown.edu>
2025-12-25 10:37:04 -0500

    Docstrings added

--- project 2 - basic skincare ingredients explaination.py
+++ project 2 - basic skincare ingredients explaination.py
@@ -1,21 +1,21 @@
 
 def skincare(i):
-"""Return a short explanation for a skincare ingredients.
+  """Return a short explanation for a skincare ingredients.
 Args:
     ingredient: Name of the ingredient (str)
 
 Returns:
     Explanation string if known, otherwise None.
-"""
-    if i == "Vitamin C":
-        return "Vitamin C brightens your skin"
-    if i == "Retinol":
-        return "Retinol reduces your wrinkles"
-    if i == "Hyaluronic Acid":
-        return "Hyaluronic Acid moisturizes your skin"
-    if i == "Niacinamide":
-        return "Niacinamide brightens your skin"
-    else:
-        return "Try again! Try typing 'Vitamin C', 'Retinol' etc"
+  """
+  if i == "Vitamin C":
+    return "Vitamin C brightens your skin"
+  if i == "Retinol":
+    return "Retinol reduces your wrinkles"
+  if i == "Hyaluronic Acid":
+    return "Hyaluronic Acid moisturizes your skin"
+  if i == "Niacinamide":
+    return "Niacinamide brightens your skin"
+  else:
+    return "Try again! Try typing 'Vitamin C', 'Retinol' etc"
 
 skincare("youself")
--- project-1 basic skincare recommendation system.py
+++ project-1 basic skincare recommendation system.py
@@ -2,6 +2,7 @@
 name = input("What is your name? ")
 age = input("How old are you? ")
 goal = input("What is your skincare goal? ")
+
 # Provide skincare recommendation based on their goal
 recommendations = {
 "brighten" : "Vitamin C or Niacinamide",
